b1e21b7071171d4e5686121c112eba9d93631f9f
Jon Duncan <jmduncan@unc.edu>
2012-11-29 14:45:09 -0500

    Added comments on what mean.py is doing.

--- mean.py
+++ mean.py
@@ -2,9 +2,11 @@
 import sys
 sum= 0
 
+#if i forget to add any arguments after mean.py, exit the program
 if len(sys.argv) == 1:
 	print'Error: No arguments given.'
 	exit()
+# do a for loop of arguments from 1 (which is the 2nd in python) to the end (:) and then average it based on the number of arguments -1 (which is the program name)
 
 for num in sys.argv[1:]:
 	sum += float(num)
