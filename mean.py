cc0c57a539510001d7f5480a7b2c487c35251e3d
Jon Duncan <jmduncan@unc.edu>
2012-11-29 14:24:00 -0500

    Added purpose of program and handled division by zero.

--- mean.py
+++ mean.py
@@ -1,5 +1,10 @@
+#Print the mean of the numbers given as arguments
 import sys
 sum= 0
+
+if len(sys.argv) == 1:
+	print'Error: No arguments given.'
+	exit()
 
 for num in sys.argv[1:]:
 	sum += float(num)
